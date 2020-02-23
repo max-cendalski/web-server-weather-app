f22dd4604de0b7516559c6cb547a45f19eed4513
Korneliu <olbiton@gmail.com>
2020-02-22 23:22:05 -0800

    added windspeed and ozone level

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. Windspeed is ' + body.currently.windSpeed + ' Ozone level is ' + body.currently.ozone + ' DU')
         }
     })
 }
